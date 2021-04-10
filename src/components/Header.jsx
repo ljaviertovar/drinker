d23b8010a812f1e285ad2a4e4d93e7619ed19e98
ljaviertovar <luisjavier_tovar@outlook.com>
2021-04-09 23:40:20 -0500

    :art: Initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+    return ( 
+
+        <header>
+            <h1>Look up drink recieps</h1>
+        </header>
+
+    );
+}
+ 
+export default Header;
